e7262efce31de65d47793a68a25889b737803135
Petra <willstilltryingit@gmail.com>
2023-12-04 18:13:33 +0100

    ok

---
+++ ukol_3.py
@@ -0,0 +1,15 @@
+import json
+
+
+with open ("body.json", mode= "r", encoding="utf-8") as slozka:
+    studenti = json.load(slozka)
+prospech = {}
+
+for jmeno, znamka in studenti.items:
+    if int(znamka) < 60:
+        prospech[jmeno] = "Fail" 
+    else:
+        prospech[jmeno] = "Pass"    
+
+with open ("prospech.json", mode= "w", encoding="utf-8") as slozka2:
+    json.dump(prospech, slozka2, ensure_ascii = False)
