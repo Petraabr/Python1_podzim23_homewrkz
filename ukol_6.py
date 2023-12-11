3dd8269a725ce9a2e8c15d01ebcefa44ac064681
Petra <willstilltryingit@gmail.com>
2023-12-12 00:19:00 +0100

    chyba

--- ukol_6.py
+++ ukol_6.py
@@ -1,7 +1,7 @@
 class Auto:
 
     def __init__(self, registracni_znacka, typ_vozidla, najete_km):
-        self.regitracni_znacka = registracni_znacka
+        self.registracni_znacka = registracni_znacka
         self.typ_vozidla = typ_vozidla
         self.najete_km = najete_km
         self.vypujceno = True
@@ -22,18 +22,18 @@ druhe_auto = Auto("1P3 4747", "Škoda Octavia", 41253)
 
 
 
-typ_vozidla = input("Které auo si chete vypůjčit: ")
+typ_vozidla = input("Které auo si chcete vypůjčit: ")
 print(typ_vozidla)
     
 
 if typ_vozidla == "Peugeot":
     print(prvni_auto.get_info())
     print(prvni_auto.pujc_auto())
-    print(prvni_auto.pujc_auto()) 
+    
     
 if typ_vozidla == "Škoda":
     print(druhe_auto.get_info())
     print(druhe_auto.pujc_auto())
-    print(druhe_auto.pujc_auto())
+   
 else:
     print("Zadej Škoda nebo Peugeot.")
