c2d2d863afd644bb1786415d9fc8d47861de015a
Petra <willstilltryingit@gmail.com>
2023-12-13 18:16:34 +0100

    nevim

--- ukol_6.py
+++ ukol_6.py
@@ -1,20 +1,21 @@
 class Auto:
 
     def __init__(self, registracni_znacka, typ_vozidla, najete_km):
-        self.registracni_znacka = registracni_znacka
+        self.regitracni_znacka = registracni_znacka
         self.typ_vozidla = typ_vozidla
         self.najete_km = najete_km
-        self.vypujceno = True
+        self.dostupnost = True
+       
 
     def pujc_auto(self):
-        if self.vypujceno:
-            self.vypujceno = False
+        if self.dostupnost:
+            self.dostupnost = False
             return ("Vozidlo bylo vypůjčeno.")
         else:
             return("Vozidlo je nedostupné.")
 
     def get_info(self):
-        return (f"Registrační značka auta: {self.registracni_znacka}.")
+        return (f"Registrační značka auta: {self.regitracni_znacka}, typ vozidla: {self.typ_vozidla}, najeté km: {self.najete_km}.")
 
 
 prvni_auto = Auto("4A2 3020", "Peugeot 403 Cabrio", 47534)
@@ -22,18 +23,29 @@ druhe_auto = Auto("1P3 4747", "Škoda Octavia", 41253)
 
 
 
-typ_vozidla = input("Které auo si chcete vypůjčit: ")
+typ_vozidla = input("Které auo si chete vypůjčit: ")
 print(typ_vozidla)
     
-
-if typ_vozidla == "Peugeot":
-    print(prvni_auto.get_info())
-    print(prvni_auto.pujc_auto())
+if typ_vozidla == "Peugeot" or typ_vozidla == "Škoda":
+    if typ_vozidla == "Peugeot":
+        print(prvni_auto.get_info())
+        print(prvni_auto.pujc_auto())
     
+    if typ_vozidla == "Škoda":
+        print(druhe_auto.get_info())
+        print(druhe_auto.pujc_auto())
     
-if typ_vozidla == "Škoda":
-    print(druhe_auto.get_info())
-    print(druhe_auto.pujc_auto())
-   
 else:
     print("Zadej Škoda nebo Peugeot.")
+
+
+
+
+
+
+
+
+
+
+    
+    
